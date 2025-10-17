71f214f95fc73a27c32e3b855735cd966940ad3c
Nndokamal <164462130+Nndokamal@users.noreply.github.com>
2025-10-17 11:22:11 +0700

    Add files via upload

--- php CRUD/halamanUtama.php
+++ php CRUD/halamanUtama.php
@@ -1,6 +1,72 @@
 <?php
 include "connect.php";
 $db = koneksidatabase();
+function tampilsemua(){
+  global $db;
+  $sql = "SELECT * FROM kumpulantabel";
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  $hasildata = $stmt->fetchall();
+  echo "<table><tr>
+      <th>nama</th>
+      <th>noinduk</th>
+      <th>jabatan</th>
+      <th>status</th>
+      <th>tglmasuk</th>
+      <th>file</th>
+      <th>edit</th>
+      <th>delete</th>
+    </tr>";
+  foreach($hasildata as $tabel){
+      echo "
+    <tr>
+      <th>$tabel[nama]</th>
+      <th>$tabel[noinduk]</th>
+      <th>$tabel[jabatan]</th>
+      <th>$tabel[status]</th>
+      <th>$tabel[tglmasuk]</th>
+      <th><img src='uploads/{$tabel['file']}' alt='$tabel[file]' width='100px'></th>
+      <th><a href='edit.php?nama={$tabel['nama']}&noinduk={$tabel['noinduk']}&jabatan={$tabel['jabatan']}&status={$tabel['status']}&tglmasuk={$tabel['tglmasuk']}&file={$tabel['file']}'>Edit</a></th>
+      <th><a href='hapus.php?nama={$tabel['nama']}&noinduk={$tabel['noinduk']}&jabatan={$tabel['jabatan']}&status={$tabel['status']}&tglmasuk={$tabel['tglmasuk']}&file={$tabel['file']}'>Hapus</a></th>
+    </tr>";
+
+  };
+  echo "</table>";
+  $db = null;
+}
+function caritampil($datacari){
+  global $db;
+  $sql = "SELECT * FROM kumpulantabel WHERE nama = '$datacari'";
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  $hasildata = $stmt->fetchall();
+  $kumpulantabel = $db->query($sql);
+  echo "<table><tr>
+      <th>nama</th>
+      <th>noinduk</th>
+      <th>jabatan</th>
+      <th>status</th>
+      <th>tglmasuk</th>
+      <th>file</th>
+      <th>edit</th>
+      <th>delete</th>
+    </tr>";
+  foreach($kumpulantabel as $tabel){
+      echo "
+    <tr>
+      <th>$tabel[nama]</th>
+      <th>$tabel[noinduk]</th>
+      <th>$tabel[jabatan]</th>
+      <th>$tabel[status]</th>
+      <th>$tabel[tglmasuk]</th>
+      <th><img src='uploads/{$tabel['file']}' alt='$tabel[file]' width='100px'></th>
+      <th><a href='edit.php?nama={$tabel['nama']}&noinduk={$tabel['noinduk']}&jabatan={$tabel['jabatan']}&status={$tabel['status']}&tglmasuk={$tabel['tglmasuk']}&file={$tabel['file']}'>Edit</a></th>
+      <th><a href='hapus.php?nama={$tabel['nama']}&noinduk={$tabel['noinduk']}&jabatan={$tabel['jabatan']}&status={$tabel['status']}&tglmasuk={$tabel['tglmasuk']}&file={$tabel['file']}'>Hapus</a></th>
+    </tr>";
+  };
+  echo "</table>";
+  $db = null;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,41 +102,42 @@ $db = koneksidatabase();
         <input type="file" name="file" id="file"></br>
         <button type="submit">simpan</button>
         <h1>kumpulan data</h1>
+    </form>   
+    <form action="" method="POST">
+      <label for="nama">nama:</label>
+      <input type="text" name="nama" id="nama">
+      <button>cari/refresh</button>
     </form>
 <?php
-$sql = "SELECT * FROM kumpulantabel";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$hasildata = $stmt->fetchall();
-$kumpulantabel = $db->query($sql);
-echo "<table><tr>
-    <th>nama</th>
-    <th>noinduk</th>
-    <th>jabatan</th>
-    <th>status</th>
-    <th>tglmasuk</th>
-    <th>file</th>
-    <th>edit</th>
-    <th>delete</th>
-  </tr>";
-foreach($kumpulantabel as $tabel){
-    echo "
-  <tr>
-    <th>$tabel[nama]</th>
-    <th>$tabel[noinduk]</th>
-    <th>$tabel[jabatan]</th>
-    <th>$tabel[status]</th>
-    <th>$tabel[tglmasuk]</th>
-    <th><img src='uploads/{$tabel['file']}' alt='$tabel[file]' width='100px'></th>
-    <th><a href='edit.php?nama={$tabel['nama']}&noinduk={$tabel['noinduk']}&jabatan={$tabel['jabatan']}&status={$tabel['status']}&tglmasuk={$tabel['tglmasuk']}&file={$tabel['file']}'>Edit</a></th>
-    <th><a href='hapus.php?nama={$tabel['nama']}&noinduk={$tabel['noinduk']}&jabatan={$tabel['jabatan']}&status={$tabel['status']}&tglmasuk={$tabel['tglmasuk']}&file={$tabel['file']}'>Hapus</a></th>
-  </tr>";
-
-};
-echo "</table>";
-
-
-$db = null;
+if($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['nama'])){
+  $hasilcari = $_POST['nama'];
+  $ada = false;
+  global $db;
+  $sql = "SELECT nama FROM kumpulantabel";
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  $hasildata = $stmt->fetchall();
+  $kumpulantabel = $db->query($sql);
+  foreach($kumpulantabel as $cari){
+    $data;
+    if ($cari['nama'] == $hasilcari){
+      global $data;
+      $data = true;
+      break;
+    } else {
+      global $data;
+      $data = false;
+    } 
+} 
+if ($data){
+  caritampil($hasilcari);
+} else{
+  echo "tidak ada data yang dicari";
+  tampilsemua();
+}
+} else{
+  tampilsemua();
+}
 ?>
 </body>
 </html>
